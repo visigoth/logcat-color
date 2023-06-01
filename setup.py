86d10c11c612365c88b49b5f61f7284040e0dca3
Shaheen Gandhi <shaheen@fb.com>
2023-06-01 12:37:44 -0700

    Make setup.py work with python3/pip3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup_data = json.load(open("setup.json", "r"))
 
 # real classy distutils, name / version have to be ascii encoded :(
 for ascii_key in ("name", "version"):
-    setup_data[ascii_key] = setup_data[ascii_key].encode("ascii")
+    setup_data[ascii_key] = setup_data[ascii_key].encode("ascii").decode()
 
 setup_data["packages"] = find_packages()
 setup(**setup_data)
